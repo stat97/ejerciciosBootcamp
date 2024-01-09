da1842f0f0c65227f876bd55d9397cadff824ed6
Adrian <ortizdev@proton.me>
2024-01-09 16:32:30 +0100

    Añado Iteración 1

---
+++ JS/basicAlgorithms/app.js
@@ -0,0 +1,32 @@
+//!Guía de práctica//
+
+//?Iteración #1: Variables//
+//*1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+let myFavoriteHero; //declaro variable usando let
+myFavoriteHero = "Hulk"; //le asigno un string como valor usando ""
+console.log("Mi primer variable string es",myFavoriteHero)
+
+//*1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+let x;
+x = 50;
+
+//*1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+let h, y;
+h = 5;
+y = 10;
+console.log("Las variables del tercer apartado son ",h,"y",y);
+//*1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'
+let z;
+z = h + y;
+console.log("La suma de las variables anteriores es igual a ",z);
+
+//?Iteración #2: Variables avanzadas//
+
+//?Iteración #3: Operadores//
+
+//?Iteración #4: Arrays//
+
+//?Iteración #5: Condicionales//
+
+//?Iteración #6: Bucles//
