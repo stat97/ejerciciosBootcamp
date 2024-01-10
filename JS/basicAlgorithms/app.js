2e4f82ba79a0b321f62e194bc992289d14a5caf0
Adrian <ortizdev@proton.me>
2024-01-10 12:02:23 +0100

    cambio enunciado

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -26,16 +26,19 @@ console.log("La suma de las variables anteriores es igual a ",z);
 const character = {name: 'Jack Sparrow', age: 25};
 
 //*1.2 Declara 3 variables con los nombres y valores siguientes 
-	/*firstName = 'Jon'; 
-	//*lastName = 'Snow'; 
-	//*age = 24; */
+	firstName = "Jon"; 
+	lastName = "Snow"; 
+	let age = 24; 
 //*Muestralos por consola de esta forma: 
 	//*'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+console.log("Hola soy",firstName,lastName,",tengo",age,"años y me gustan los lobos.")
 
 //*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 //*ambos juguetes.
-//*const toy1 = {name: 'Buss myYear', price: 19};
-//*const toy2 = {name: 'Rallo mcKing', price: 29};
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+const finalPrice = (toy1,toy2) => toy1+toy2;
+console.log("El precio total de ambos productos es de",finalPrice)
 
 //*1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 //*y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
