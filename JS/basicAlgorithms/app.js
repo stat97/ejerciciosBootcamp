24905224004a9b2a2fb88f75091382589acdcc57
Adrian <ortizdev@proton.me>
2024-01-10 13:40:05 +0100

    Iteration#4

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -73,7 +73,32 @@ let m=5;
 let i= c*m;
 console.log(i)
 //?Iteración #4: Arrays//
+console.log("Iteración #4: Arrays")
+//*1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0])
 
+//*1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers2 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers2[0]="IRONMAN"
+console.log(avengers2)
+//*1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+const avengers3 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers3.length)
+//*1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+//*Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+console.log(rickAndMortyCharacters[2])
+
+//*1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop() //elimino ultimo elemento
+console.log(rickAndMortyCharacters2[0],"and",rickAndMortyCharacters2[4])
+
+//*1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+delete rickAndMortyCharacters3[1];
+console.log(rickAndMortyCharacters3)
 //?Iteración #5: Condicionales//
 
 //?Iteración #6: Bucles//
