97ee66e5d291f1981b5a2aeccbdce1152ae24b87
Adrian <ortizdev@proton.me>
2024-01-10 15:36:12 +0100

    Iteration#6

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -137,3 +137,27 @@ if (number2/2 == number1 || number1/5 == number3) {
 }
 
 //?Iteración #6: Bucles//
+console.log("Iteración #6: Bucles")
+//*1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+for (let i = 0; i <= 9; i++) {
+	console.log(i);
+  }
+//*1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+//*cuando el resto del numero dividido entre 2 sea 0.
+for (let i = 0; i <= 9; i++) {
+	if( i%2 == 0)(
+	console.log(i))
+  }
+
+//*1.3 Crea un bucle para conseguir dormir contando ovejas. 
+//*Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+//*Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+//*y cambia el mensaje en la décima vuelta a 'Dormido!'.
+for (let i = 0; i <= 9; i++) {
+	if (i< 9 )(
+	console.log("Intentando dormir 🐑"))
+	else (
+		console.log("Dormido!")
+	)
+	;
+  }
