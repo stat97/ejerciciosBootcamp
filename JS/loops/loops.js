02f102572dc2feb88f636896b9ef86b126c1d4e0
Adrian <ortizdev@proton.me>
2024-01-15 17:04:45 +0100

    Iteration#3

--- JS/loops/loops.js
+++ JS/loops/loops.js
@@ -46,3 +46,13 @@ return aprobado
 let aprobadoFinal = comprobarTrimestres(alumns)
 console.log (`Los alumnos con almenos dos trimestres aprobados son ${aprobadoFinal}`)
 
+//**Iteración #3: Probando For...of**
+//Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.
+//Puedes usar este array:
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const iterator of placesToTravel) {
+  console.log(iterator)
+}
+
+
