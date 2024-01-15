93ff357378126ef282b274403d04f766e9b0f91b
Adrian <ortizdev@proton.me>
2024-01-15 18:45:32 +0100

    iteration#7

--- JS/loops/loops.js
+++ JS/loops/loops.js
@@ -101,6 +101,24 @@ for (const iterator of toys) {
   if(!iterator.name.includes('gato')){
     freeToys.push(iterator);
   }
-  
 }
 console.log(freeToys)
+
+//**Iteración #7: For...of avanzado**
+
+//*Usa un bucle **for...of** para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:
+const popularToys = [];
+const toysx = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+for (const iterator of toysx) {
+  if(iterator.sellCount> 15){
+    popularToys.push(iterator)
+  }
+  
+}
+console.log(popularToys)
