dcb9132b6df61d38f125c09c8f72b2ecdeb455ee
Adrian <ortizdev@proton.me>
2024-01-16 16:01:37 +0100

    Iteration#3

--- JS/reviewBasics/reviewBasics.js
+++ JS/reviewBasics/reviewBasics.js
@@ -49,7 +49,7 @@ const users = [
         }
     },
 ]
-//for of para iterar el objeto y for in para iterar dentro del objeto una propiedad
+//for...of se utiliza para iterar sobre los elementos del array users, y for...in se utiliza para iterar sobre las propiedades del objeto
 const newSounds=[];
 let volumenTotal=0 ;
 for (const clave of users) {
@@ -62,4 +62,56 @@ for (const clave of users) {
     }
 }
 console.log(`La media de volumen entre todos los usuarios es de ${volumenTotal/newSounds.length}`)
+//**Iteración #3: Mix Fors**
+//*Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
+//*Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+//*Este ejercicio es un poco complicado con los conocimientos actuales pero...a la vez un buen reto y oportunidad para comprender que hay muchas formas de hacer las cosas en javascript.
+
+
+const users1 = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+const newSounds1 = [];
+for (const clave of users) {
+  for (const key in clave.favoritesSounds) {
+    if (clave.favoritesSounds.hasOwnProperty.call(clave.favoritesSounds, key)) {
+      const element = clave.favoritesSounds[key];
+      if (!newSounds1[key]) {
+        newSounds1[key] = 1; //si no esta el sonido favorito , es la primera vez que lo encuentra añade 1
+      } else {
+        newSounds1[key]++; //si esta repetido se le suma 1
+      }
+    }
+  }
+}
+console.log(newSounds1);
+
+
 
