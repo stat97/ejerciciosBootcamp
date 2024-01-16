9580d8c80b84926902b21ede53a9e993d5564fff
Adrian <ortizdev@proton.me>
2024-01-16 15:21:54 +0100

    Iteration#2

--- JS/reviewBasics/reviewBasics.js
+++ JS/reviewBasics/reviewBasics.js
@@ -16,3 +16,50 @@ for (const category of iterator.categories) {
 }
 }
 console.log(newMovies)
+//**Iteración #2: Mix Fors**
+
+//*Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+//for of para iterar el objeto y for in para iterar dentro del objeto una propiedad
+const newSounds=[];
+let volumenTotal=0 ;
+for (const clave of users) {
+    for (const key in clave.favoritesSounds) {
+        if (clave.favoritesSounds.hasOwnProperty.call(clave.favoritesSounds, key)) {
+            const element  = clave.favoritesSounds[key];
+            newSounds.push(element);
+            volumenTotal += element.volume
+        }
+    }
+}
+console.log(`La media de volumen entre todos los usuarios es de ${volumenTotal/newSounds.length}`)
+
