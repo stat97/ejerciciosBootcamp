361c5b219114bf4d8b1768228eb62ac64d33f05f
unknown <ortizdev@proton.me>
2024-01-17 15:38:23 +0100

    Iteration#2_2_1

--- JS/ES6/es6.js
+++ JS/ES6/es6.js
@@ -13,3 +13,13 @@ console.log(newCall);
 //*1.3 Ejecuta esta función pasando dos parametros
 const secondCall = arrowFunction(6, 24);
 console.log(secondCall);
+//**Iteración #2: Destructuring**
+
+
+//*2.1 En base al siguiente javascript, crea variables en base a las propiedades 
+//*del objeto usando object destructuring e imprimelas por consola. Cuidado, 
+//*no hace falta hacer destructuring del array, solo del objeto.
+
+const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+const {title , gender ,year} = game
+console.log(`title: ${title} , gender: ${gender}, year: ${year}`)
