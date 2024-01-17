52ce51451bbb085e9c35efe6af82bf1cfb3bdcfe
unknown <ortizdev@proton.me>
2024-01-17 15:47:54 +0100

    Iteration#2_2_3

--- JS/ES6/es6.js
+++ JS/ES6/es6.js
@@ -15,18 +15,30 @@ const secondCall = arrowFunction(6, 24);
 console.log(secondCall);
 //**Iteración #2: Destructuring**
 
-
-//*2.1 En base al siguiente javascript, crea variables en base a las propiedades 
-//*del objeto usando object destructuring e imprimelas por consola. Cuidado, 
+//*2.1 En base al siguiente javascript, crea variables en base a las propiedades
+//*del objeto usando object destructuring e imprimelas por consola. Cuidado,
 //*no hace falta hacer destructuring del array, solo del objeto.
 
-const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
-const {title , gender ,year} = game
-console.log(`title: ${title} , gender: ${gender}, year: ${year}`)
-//*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
+const game = {
+  title: "The last us 2",
+  gender: ["action", "zombie", "survival"],
+  year: 2020,
+};
+const { title, gender, year } = game;
+console.log(`title: ${title} , gender: ${gender}, year: ${year}`);
+//*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables
 //*llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 //*imprimelo por consola.
 
-const fruits = ['Banana', 'Strawberry', 'Orange'];
-const [fruit1,fruit2,fruit3] = fruits
-console.log(`${fruit1}, ${fruit2} y ${fruit3}`)
+const fruits = ["Banana", "Strawberry", "Orange"];
+const [fruit1, fruit2, fruit3] = fruits;
+console.log(`${fruit1}, ${fruit2} y ${fruit3}`);
+//*2.3 En base al siguiente javascript, usa destructuring para crear 2 
+//*variables igualandolo a la función e imprimiendolo por consola.
+
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+  const {name, race } = animalFunction();
+  let resultado = animalFunction();
+  console.log(resultado)
