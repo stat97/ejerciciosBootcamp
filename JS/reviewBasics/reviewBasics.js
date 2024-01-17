ebdd4e63c268e37fb62a4efb3135f1599f0b5414
unknown <ortizdev@proton.me>
2024-01-17 15:02:24 +0100

    Añadiendo mensaje consola

--- JS/reviewBasics/reviewBasics.js
+++ JS/reviewBasics/reviewBasics.js
@@ -1,4 +1,5 @@
 //**Iteración #1: Mix for e includes**
+console.log("Iteracion#1 :Mix for e includes")
 //*Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función **.includes()**
 const movies = [
     { title: "Madaraspar", duration: 192, categories: ["comedia", "aventura"] },
