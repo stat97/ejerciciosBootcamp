4256edf21e3f19449c13bae25085712eaa239723
unknown <ortizdev@proton.me>
2024-01-17 15:54:26 +0100

    Iteration#2_2_4

--- JS/ES6/es6.js
+++ JS/ES6/es6.js
@@ -42,3 +42,14 @@ const animalFunction = () => {
   const {name, race } = animalFunction();
   let resultado = animalFunction();
   console.log(resultado)
+
+//*2.4 En base al siguiente javascript, usa destructuring para crear las 
+//*variables name y itv con sus respectivos valores. Posteriormente crea 
+//*3 variables usando igualmente el destructuring para cada uno de los años 
+//*y comprueba que todo esta bien imprimiendolo.
+
+const car = {namecar: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+const{namecar,itv} = car
+const [year1,year2,year3] = itv
+console.log(`${namecar}, Años ITV ${year1},${year2} y ${year3}`)
