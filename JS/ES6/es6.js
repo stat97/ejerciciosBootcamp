381380dc247f34cb53db09306de21aacb226932b
unknown <ortizdev@proton.me>
2024-01-18 17:40:36 +0100

    Modifications & iteration#6_6_3

--- JS/ES6/es6.js
+++ JS/ES6/es6.js
@@ -214,7 +214,7 @@ function filterList2(streamers3) {
 
 //*6.1 Dado el siguiente array, usa .find() para econtrar el número 100.
 const numbers = [32, 21, 63, 95, 100, 67, 43];
-const newNumbers = numbers.find((numbers,index) => numbers === 100)
+const newNumbers = numbers.find((number,index) => number === 100)
 console.log(newNumbers)
 
 
@@ -224,6 +224,25 @@ const movies = [
 	{title: 'Origen', stars: 5, date: 2010},
 	{title: 'Your Name', stars: 5, date: 2016}
 ];
-const findMovies = movies.find((movies,index) => movies.date === 2010)
+const findMovies = movies.find((movie,index) => movie.date === 2010)
 console.log(findMovies)
 
+//*6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre 
+//*'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa 
+//*spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación 
+//*lo queremos meter en la propiedad .mutation del objeto fusionado.
+const aliens = [
+	{name: 'Zalamero', planet: 'Eden', age: 4029},
+	{name: 'Paktu', planet: 'Andromeda', age: 32},
+	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
+];
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+];
+
+const findAliens = aliens.find((alien) => alien.name === "Cucushumushu")
+const findMutations = mutations.find((mutation) => mutation.name === "Porompompero")
+objectTogether = {... findAliens, ...findMutations}
+console.log(objectTogether)
