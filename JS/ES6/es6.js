e07d7321c4eb65a2ea8ebe7b82348ad34ae7f0ca
unknown <ortizdev@proton.me>
2024-01-19 18:50:34 +0100

    Iteration#7

--- JS/ES6/es6.js
+++ JS/ES6/es6.js
@@ -246,3 +246,30 @@ const findAliens = aliens.find((alien) => alien.name === "Cucushumushu")
 const findMutations = mutations.find((mutation) => mutation.name === "Porompompero")
 objectTogether = {... findAliens, ...findMutations}
 console.log(objectTogether)
+
+//**Iteración #7: Reduce**
+
+
+//*7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+//*los alumnos usando la función .reduce().
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+//*Se mapea para obtener un array de scores
+ const exams1 = exams.map((exams,index) => exams.score)
+console.log(exams1)
+//*Reduce para la suma empezando de 0
+const sumExams = exams1.reduce((acc,num) => acc+num,0)
+
+console.log(`La suma de los scores es de ${sumExams}`)
