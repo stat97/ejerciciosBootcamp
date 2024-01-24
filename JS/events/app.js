609a8a753b6eeb98acab1ea79fea07759b1188de
Adrian <ortizdev@proton.me>
2024-01-24 19:21:25 +0100

    Focus_events

--- JS/events/app.js
+++ JS/events/app.js
@@ -2,4 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("btnToClick").addEventListener("click", (e) => {
         console.log(e);
     });
+    btnToClick.focus();
 });
+
