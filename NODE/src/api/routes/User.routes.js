a7e0ff57123af3457a57b98f4c1b27f7cc9eca96
Adrian <ortizdev@proton.me>
2024-02-05 15:32:36 +0100

    actualizando routes

--- NODE/src/api/routes/User.routes.js
+++ NODE/src/api/routes/User.routes.js
@@ -6,6 +6,11 @@ const {
   registerWithRedirect,
   sendCode,
   login,
+  resendCode,
+  checkNewUser,
+  autoLogin,
+  changePassword,
+  sendPassword,
 } = require("../controllers/User.controllers");
 const express = require("express");
 const UserRoutes = express.Router();
@@ -17,4 +22,10 @@ UserRoutes.get("/register", upload.single("image"), registerWithRedirect);
 UserRoutes.post("/login", login);
 /// ------------------> rutas que pueden ser redirect
 UserRoutes.get("/register/sendMail/:id", sendCode); // :id ---> es el nombre del param
+
+serRoutes.post("/resend", resendCode);
+UserRoutes.post("/check", checkNewUser);
+UserRoutes.post("/login/autologin", autoLogin);
+UserRoutes.patch("/forgotpassword", changePassword);
+UserRoutes.patch("/sendPassword/:id", sendPassword);
 module.exports = UserRoutes;
